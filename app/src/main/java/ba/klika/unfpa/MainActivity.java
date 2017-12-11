e570d4292dd7a970f31a3b8b10030713e467bc59
esensar <es.ensar@gmail.com>
2017-12-11 12:41:09 +0100

    Add mobile center sdk

--- app/src/main/java/ba/klika/unfpa/MainActivity.java
+++ app/src/main/java/ba/klika/unfpa/MainActivity.java
@@ -1,8 +1,13 @@
 package ba.klika.unfpa;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         } else {
             webView.loadUrl("https://unfpa-official.herokuapp.com/");
         }
+
+        AppCenter.start(getApplication(), BuildConfig.APP_CENTER_SECRET,
+                Analytics.class, Crashes.class);
     }
 
     @Override
